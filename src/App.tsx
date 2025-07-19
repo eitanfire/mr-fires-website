1147e21b8b8f9deb8409d84da19911dac761d751
Eitan Fire <eitanfire@gmail.com>
2025-07-19 14:12:26 -0600

    add HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import HomePage from "./pages/HomePage";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}><HomePage /></MantineProvider>;
 }
