aac329c16dc05e45be8e71b34cf09084c4565fe6
Eitan Fire <eitanfire@gmail.com>
2025-07-19 22:52:03 -0600

    add deployment to firebase

---
+++ src/app/utils/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDViGxwuOBs2X9KqezYHon17PaQ6q_Ts6c",
+  authDomain: "mr-fires-ja-website.firebaseapp.com",
+  projectId: "mr-fires-ja-website",
+  storageBucket: "mr-fires-ja-website.firebasestorage.app",
+  messagingSenderId: "905901835531",
+  appId: "1:905901835531:web:1d6e79e91555b6ad390e0e",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
