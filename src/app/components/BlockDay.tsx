1d851279594b84ef91d10df38ea12ec2e2f05a27
Eitan Fire <eitanfire@gmail.com>
2025-07-23 17:37:14 -0600

    add BlockDay

--- src/app/components/BlockDay.tsx
+++ src/app/components/BlockDay.tsx
@@ -1,4 +1,46 @@
-const BlockDay = () => {
-  return <div>Block Day</div>;
-}
+import { useState, useEffect } from "react";
+import { Container, Grid } from "@mantine/core";
+import { useSpring, animated } from "react-spring";
+import weekday from "../utils/day";
+import aDayBDay from "../utils/aDayBDay";
+
+interface BlockDayProps {}
+
+const BlockDay: React.FC<BlockDayProps> = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const animatedStyle = useSpring({
+    opacity: toggle ? 1 : 0,
+    transform: toggle ? "scale(1,1)" : "scale(1,0)",
+    config: { duration: 500 },
+  });
+
+  useEffect(() => {
+    setToggle(true);
+  }, []);
+
+  const brightDay: JSX.Element = (
+    <span id="aBAB">
+      {typeof aDayBDay === "string" ? aDayBDay.slice(1) : ""}
+    </span>
+  );
+
+  return (
+    <animated.div style={animatedStyle}>
+      <Container className="BlockDay">
+        <Grid className="BlockDay-text">
+          <Grid.Col span={12}>
+            <span>Today is {weekday}.</span>
+          </Grid.Col>
+          {aDayBDay && (
+            <Grid.Col span={12}>
+              It's a{aDayBDay[0]} {brightDay} day.
+            </Grid.Col>
+          )}
+        </Grid>
+      </Container>
+    </animated.div>
+  );
+};
+
 export default BlockDay;
---
+++ src/app/utils/aDayBDay.ts
@@ -0,0 +1,5 @@
+const aDayBDay = [false, "n A", " B", "n A", " B", "n A", false];
+const d = new Date();
+let block = aDayBDay[d.getDay()];
+
+export default block;
