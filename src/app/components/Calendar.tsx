70aa85fea0fdc42b8f9ee6116d4693035e7bcca8
Eitan Fire <eitanfire@gmail.com>
2025-07-31 22:12:05 -0600

    add Calendar

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,4 +1,33 @@
-const Calendar = () => {
-  return <div>Calendar</div>;
+import { Box, BoxProps } from '@mantine/core';
+
+interface CalendarProps extends BoxProps {
+  iframeSource?: string;
+  height?: number | string;
+  width?: number | string;
 }
-export default Calendar;
+
+const DEFAULT_IFRAME_SOURCE = `<iframe 
+  src="https://calendar.google.com/calendar/embed?src=en.usa%23holiday%40group.v.calendar.google.com&ctz=America%2FDenver" 
+  style="border:solid 1px #777" 
+  width="100%" 
+  height="300" 
+  frameborder="0" 
+  scrolling="no">
+</iframe>`;
+
+export default function Calendar({ 
+  iframeSource = DEFAULT_IFRAME_SOURCE,
+  height = 300,
+  width = '100%',
+  ...boxProps 
+}: CalendarProps) {
+  return (
+    <Box
+      className="Calendar"
+      w={width}
+      h={height}
+      dangerouslySetInnerHTML={{ __html: iframeSource }}
+      {...boxProps}
+    />
+  );
+}
