a14daea1f82a017477b68b545a2925ab19b59e9b
Eitan Fire <eitanfire@gmail.com>
2025-08-03 10:38:32 -0600

    format

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ export default function App() {
         <Route path="read-more" element={<FiresIntro />} />
         <Route path="contact" element={<ContactForm />} />
         <Route path="shout-out" element={<MakeAShoutOutForm />} />
-      </Routes>            <Footer />
+      </Routes>            
+      <Footer />
 </MantineProvider>;
 }
