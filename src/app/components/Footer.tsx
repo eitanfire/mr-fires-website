08f1a0d11515a1b68f3901557200c7941ac22e91
Eitan Fire <eitanfire@gmail.com>
2025-08-11 23:35:32 -0600

    add credits

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Group, Text } from "@mantine/core";
+import { Center, Group, Text } from "@mantine/core";
 import getCurrentYear from "../utils/getCurrentYear";
 
 const Footer: React.FC = () => {
   return (
     <Group className="footer" justify="center" gap="xs" >
+      <Center>
+        <p>
       <Text c="school.7" component="span"   style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
 >
         &nbsp;©{getCurrentYear()}&nbsp;| Eitan Fire&nbsp;
       </Text>
+      <Text>Credit for images to ID 133420539 © Marina Vorontsova | Dreamstime.com & ID 170510354 © Kolgotki23 | Dreamstime.com</Text>
+      </p>
+      </Center>      
     </Group>
   );
 };
